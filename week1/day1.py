ee7b862107c58c109cf702ee9a79c0d9128d4cca
jenny-jlp <1404489426@qq.com>
2026-05-12 17:29:24 +0800

    finish python basic day1

---
+++ week1/day1.py
@@ -0,0 +1,20 @@
+print('Hello AI')
+name = 'jenny'
+age = 22
+
+print('My name is',name)
+print('My age is',age)
+
+score = 85
+if score >= 60:
+    print('Pass')
+else:
+    print('Fail')
+
+for i in range(5):
+    print(i)
+
+def add(a, b):
+    return a + b
+result = add(3, 5)
+print('The sum is', result)
